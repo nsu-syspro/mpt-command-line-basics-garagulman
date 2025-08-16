d4518e97a772d66e94ea6f45ee10908d0bc6db4a
garagulman <akgaragulova@gmail.com>
2025-08-16 17:08:25 +0700

    use only one tmp

--- solution/task1.sh
+++ solution/task1.sh
@@ -1,6 +1,4 @@
-mkdir dataCur 
-mkdir dataCur/tar
-mkdir dataCur/zip 
-tar -xf data/archive-part1.tar -C dataCur/tar 
-unzip data/archive-part2.zip -d dataCur/zip
-tar -czvf data/archive-combined.tar.gz dataCur/
+mktemp -d 
+tar -xf data/archive-part1.tar -C tmp 
+unzip data/archive-part2.zip -d tmp
+tar -czvf data/archive-combined.tar.gz tmp/some/
