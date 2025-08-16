4c777fbbf0e7aef8dc9bdebc3a003545021bd7ee
garagulman <akgaragulova@gmail.com>
2025-08-16 18:48:15 +0700

    task 2 add type f

--- solution/task2.sh
+++ solution/task2.sh
@@ -1,3 +1,3 @@
-find -name foo.txt -exec cat {} +
-find -name bar.txt -exec cat {} +
-find -name baz.txt -exec cat {} +
+find -type f -name foo.txt -exec cat {} +
+find -type f -name bar.txt -exec cat {} +
+find -type f -name baz.txt -exec cat {} +
