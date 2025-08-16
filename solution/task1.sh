8cb1dfda0f392d2eaa11a8840d5e50026824707f
garagulman <akgaragulova@gmail.com>
2025-08-16 17:55:00 +0700

    add cd

--- solution/task1.sh
+++ solution/task1.sh
@@ -1,4 +1,5 @@
 mktemp -d 
 tar -xf data/archive-part1.tar -C tmp 
 unzip data/archive-part2.zip -d tmp
-tar -czvf data/archive-combined.tar.gz tmp/some/
+cd tmp
+tar -czvf ../data/archive-combined.tar.gz some/
