924e46264c952400d4f3f299bb14e188c69b3025
garagulman <akgaragulova@gmail.com>
2025-08-16 14:50:25 +0700

    sdfsdf

---
+++ solution/task1.sh
@@ -0,0 +1,7 @@
+cd ..  
+mkdir dataCur 
+mkdir dataCur/tar
+mkdir dataCur/zip 
+tar -xf data/archive-part1.tar -C dataCur/tar 
+unzip data/archive-part2.zip -d dataCur/zip
+tar -czvf data/archive-combined.tar.gz dataCur/
