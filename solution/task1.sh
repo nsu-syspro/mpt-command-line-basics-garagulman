f4831d74908369aa4e417291f8a5f2c90415ab79
garagulman <akgaragulova@gmail.com>
2025-08-16 18:29:29 +0700

    change order

--- solution/task1.sh
+++ solution/task1.sh
@@ -1,5 +1,5 @@
-mktemp -d 
-tar -xf data/archive-part1.tar -C tmp 
+mktemp -d  
 unzip data/archive-part2.zip -d tmp
+tar -xf data/archive-part1.tar -C tmp
 cd tmp
 tar -czvf ../data/archive-combined.tar.gz some/
